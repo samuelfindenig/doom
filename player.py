1061fd8357c662914513cd2119cad7d55833d026
samuelfindenig <146440803+samuelfindenig@users.noreply.github.com>
2024-03-28 09:22:45 +0100

    Player can turn

--- player.py
+++ player.py
@@ -16,35 +16,35 @@ class Player:
         speed_sin = speed * sin_a
         speed_cos = speed * cos_a
 
-        key = pg.key.get_pressed()
-        if key[pg.K_d]:
+        keys = pg.key.get_pressed()
+        if keys[pg.K_d]:
             dx = dx + speed_cos
             dy = dy + speed_sin
-        if key[pg.K_a]:
+        if keys[pg.K_a]:
             dx = dx - speed_cos
             dy = dy - speed_sin
-        if key[pg.K_w]:
+        if keys[pg.K_w]:
             dx = dx + speed_sin
             dy = dy - speed_cos
-        if key[pg.K_s]:
+        if keys[pg.K_s]:
             dx = dx - speed_sin
             dy = dy + speed_cos
 
         self.x += dx
         self.y += dy
 
-        if key[pg.K_LEFT]:
-            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
-        if key[pg.K_RIGHT]:
-            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
-        self.angel %= math.pi * 2
+        if keys[pg.K_LEFT]:
+            self.angel -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angel += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angel %= math.tau
 
     def draw(self):
         pg.draw.line(self.game.screen, "yellow", (self.x * 100, self.y * 100),
                     (self.x * 100 + WIDTH * math.cos(self.angel), 
                     self.y * 100 + WIDTH * math.sin(self.angel)), 2)
         pg.draw.circle(self.game.screen, "green", (self.x *100, self.y * 100), 15)                
-    def update(self):#
+    def update(self):
         self.movement()
     
     @property
